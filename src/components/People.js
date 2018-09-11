e73749f1892ff567f59b39e3a0c9406498e795f2
byoung730 <brian.young@monsanto.com>
2018-09-11 14:04:55 -0500

    final-form added

--- src/components/People.js
+++ src/components/People.js
@@ -1,4 +1,18 @@
 import React, { Component } from "react";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import MenuItem from "@material-ui/core/MenuItem";
+import {
+  withStyles,
+  Card,
+  CardContent,
+  CardActions,
+  Modal,
+  Button,
+  TextField
+} from "@material-ui/core";
+import { compose } from "recompose";
+import { withRouter } from "react-router-dom";
+import { Form, Field } from "react-final-form";
 
 class People extends Component {
   constructor(props) {
