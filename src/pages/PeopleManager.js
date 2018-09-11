003044eb61b2a9910db2421b5aa0d43643da0497
byoung730 <brian.young@monsanto.com>
2018-09-11 15:50:34 -0500

    mostly working

--- src/pages/PeopleManager.js
+++ src/pages/PeopleManager.js
@@ -12,11 +12,11 @@ import {
   ListItemSecondaryAction
 } from "@material-ui/core";
 import { Delete as DeleteIcon, Add as AddIcon } from "@material-ui/icons";
-import moment from "moment";
 import { find, orderBy } from "lodash";
 import { compose } from "recompose";
 
-import People from "../components/People";
+import PeopleEditor from "../components/PeopleEditor";
+// import People from "../components/People";
 
 const styles = theme => ({
   people: {
@@ -52,8 +52,7 @@ class PeopleManager extends Component {
         body: body && JSON.stringify(body),
         headers: {
           "content-type": "application/json",
-          accept: "application/json",
-          authorization: `Bearer ${await this.props.auth.getAccessToken()}`
+          accept: "application/json"
         }
       });
       return await response.json();
@@ -65,14 +64,14 @@ class PeopleManager extends Component {
   async getPeople() {
     this.setState({
       loading: false,
-      people: await this.fetch("get", "/people")
+      people: await this.fetch("get", "/api/people")
     });
   }
 
   savePeople = async people => {
     people.id = 1;
     if (people.email) {
-      await this.fetch("put", `/people/${people.length}`, people);
+      await this.fetch("post", `/people/${people.id}`, people);
     } else {
       await this.fetch("get", "/people", people);
     }
@@ -112,34 +111,28 @@ class PeopleManager extends Component {
         {this.state.people.length > 0 ? (
           <Paper elevation={1} className={classes.people}>
             <List>
-              {orderBy(
-                this.state.people,
-                ["updated_at", "last_name"],
-                ["desc", "asc"]
-              ).map(people => (
-                <ListItem
-                  key={people.person_id}
-                  button
-                  component={Link}
-                  to={`/people/${people.person_id}`}
-                >
-                  <ListItemText
-                    primary={people.first_name + " " + people.last_name}
-                    secondary={
-                      people.updated_at &&
-                      `Updated ${moment(people.updatedAt).fromNow()}`
-                    }
-                  />
-                  <ListItemSecondaryAction>
-                    <IconButton
-                      onClick={() => this.deletePeople(people)}
-                      color="inherit"
-                    >
-                      <DeleteIcon />
-                    </IconButton>
-                  </ListItemSecondaryAction>
-                </ListItem>
-              ))}
+              {orderBy(this.state.people, ["last_name"], ["asc"]).map(
+                people => (
+                  <ListItem
+                    key={people.person_id}
+                    button
+                    component={Link}
+                    to={`/people/${people.person_id}`}
+                  >
+                    <ListItemText
+                      primary={people.first_name + " " + people.last_name}
+                    />
+                    <ListItemSecondaryAction>
+                      <IconButton
+                        onClick={() => this.deletePeople(people)}
+                        color="inherit"
+                      >
+                        <DeleteIcon />
+                      </IconButton>
+                    </ListItemSecondaryAction>
+                  </ListItem>
+                )
+              )}
             </List>
           </Paper>
         ) : (
